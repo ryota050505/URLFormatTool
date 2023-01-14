88ec42e4f6b2f8b7ff4e4595b7a2e9b54093bb2c
ryota050505 <05ryouta05@gmail.com>
2023-01-14 14:37:16 +0900

    URL変換機能実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,87 +1,118 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div class="text-center">
+    <v-row
+      class="mt-10"
+      style="height: 30vh;"
+      align-content="center" justify="center"
+    >
+      <v-col
+        sm="12"
+        md="6"
+        lg="6"
+      >
+        <v-text-field
+          v-model="url"
+          label="変換前URL"
+          placeholder="https://example.com?a=1"
+          outlined
+          prepend-inner-icon="mdi-link"
+        />
+      </v-col>
+    </v-row>
+    <v-row
+      style="height: 10vh;"
+      align-content="center" justify="center"
+    >
+      <v-col
+        sm="12"
+        md="6"
+        lg="6"
+      >
+        <v-text-field
+          label="変換後URL（読み取り専用）"
+          placeholder="https://example.com"
+          outlined
+          readonly
+          :value="getFormattedUrl"
+          prepend-inner-icon="mdi-link"
+        />
+      </v-col>
+    </v-row>
+    <v-row
+      style="height: 10vh;"
+      align-content="center" justify="center"
+    >
+      <v-btn
+        color="primary"
+        :disabled="!isUrl()"
+        @click="copy"
+      >
+        COPY
+      </v-btn>
+    </v-row>
+
+    <v-snackbar
+      v-model="snackbar"
+      :timeout="3000"
+      :color="color"
+      outlined
+    >
+      {{ message }}
+      <template #action="{ attrs }">
+        <v-btn
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      url: '',
+      urlRegExp: /https?:\/\/[-_.!~*'()a-zA-Z0-9;/?:@&=+$,%#]+/g,
+      snackbar: false,
+      message: '',
+      color: '',
+    }
+  },
+  computed: {
+    getFormattedUrl() {
+      if (!this.isUrl()) return '';
+      const resultUrl = new URL(this.url);
+      const { searchParams } = resultUrl;
+      // クエリパラメータを削除
+      searchParams.forEach((_, k) => {
+        resultUrl.searchParams.delete(k);
+      })
+      const urlStr = resultUrl.origin + resultUrl.pathname + (resultUrl.pathname.at(-1) === '/' ? '' : '/');
+      return urlStr + 'video/1';
+    }
+  },
+  methods: {
+    // クリップボードにコピー
+    copy() {
+      navigator.clipboard.writeText(this.getFormattedUrl).then(
+      () => {
+        this.message = 'コピーしました。';
+        this.color = 'success';
+      },
+      () => {
+        this.message = 'コピーできませんでした';
+        this.error = 'error';
+      });
+      this.snackbar = true
+    },
+    isUrl() {
+      return this.url.match(this.urlRegExp)
+    },
+  },
 }
 </script>
